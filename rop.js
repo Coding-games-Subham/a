64488e4ac68d85bb6e5524232eacca916df6b71d
Coding-games-Subham <69688595+Coding-games-Subham@users.noreply.github.com>
2020-10-13 16:09:22 +0530

    Add files via upload

---
+++ rop.js
@@ -0,0 +1,30 @@
+class Box {
+   constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+   
+  }
+  display(){
+    fill("orange");
+   // var angle = this.body.position;
+   // rect(angle.x,angle.y,this.width,this.height)
+    if(this.body.speed < 6){
+     var angle = this.body.position;
+    rect(angle.x,angle.y,this.width,this.height)
+    }else{
+     World.remove(world,this.body);
+    
+   
+   }
+    
+    }
+  }
+
+
